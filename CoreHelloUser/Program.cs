cc351f0722800f6618b20b123a35221d2e977139
Jon Spencer <jondspen@gmail.com>
2017-08-20 07:56:52 -0500

    Initial checking of CoreHelloWorld

---
+++ CoreHelloUser/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CoreHelloUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            var userName = Console.ReadLine();
+            var dtg = DateTime.Now;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"\nHello {userName}.  The date/time is {dtg}.");
+            Console.ResetColor();
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
--- CoreHelloWorld/Program.cs
+++ CoreHelloWorld/Program.cs
@@ -7,6 +7,8 @@ namespace CoreHelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(true);
         }
     }
 }
